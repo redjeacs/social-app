cbf8be78966840075cd4a7c2db543da00878f1b7
Jerry Chang <213577976+redjeacs@users.noreply.github.com>
2026-01-27 15:46:38 +0800

    display message using MessageBubble
    - display the message with simple timestamp
    - styling changes depending on sender/receiver
    - message chain end detection not yet implemented

--- client/src/components/MessageBubble.jsx
+++ client/src/components/MessageBubble.jsx
@@ -4,24 +4,28 @@ import { useState } from "react";
 
 function MessageBubble({ message }) {
   const { user } = useAuth();
+  const [isSender, setIsSender] = useState(message.senderId === user.id);
+  const [isEnd, setIsEnd] = useState(true); // For now, assume all messages are end of chain
   const [bubbleHover, setBubbleHover] = useState(false);
   return (
     <>
       <li className="w-full">
-        {/* justify-start for friend */}
-        <div className="flex px-4 py-1 justify-end">
+        <div
+          className={`flex px-4 py-1 ${isSender ? "justify-end" : "justify-start"}`}
+        >
           <div
             onMouseOver={() => setBubbleHover(true)}
             onMouseLeave={() => setBubbleHover(false)}
             className="grid gap-x-2 gapy-y-0.5 max-w-[calc(55%+128px)] grid-cols-[auto_auto_auto_auto] items-center -me-4"
           >
-            {/* bg-(--twitter-gray-50) for friend */}
-            <div className="flex items-end px-4 py-2 rounded-2xl bg-(--twitter-blue) gap-1">
+            <div
+              className={`flex items-end px-4 py-2 rounded-2xl gap-1 ${isSender ? "bg-(--twitter-blue)" : "bg-(--twitter-gray-50)"}`}
+            >
               <span className="max-w-full leading-4.5">{message.body}</span>
               {/* hidden if not end of message chain */}
               <span
                 aria-hidden="true"
-                className="inline-block pl-2 user-select-none"
+                className={`${isEnd ? "inline-block" : "hidden"} pl-2 user-select-none`}
               >
                 <div className="flex items-center justify-end text-white ml-auto shrink-0 gap-1 max-w-full text-xs leading-3">
                   {formatTimeSimple(new Date(message.createdAt))}
@@ -30,8 +34,8 @@ function MessageBubble({ message }) {
             </div>
             {/* Remove style for friend */}
             <div
-              className="w-32 flex items-center gap-1 min-h-8 flex-row-reverse"
-              style={{ gridArea: "1" }}
+              className={`w-32 flex items-center gap-1 min-h-8 ${isSender ? "flex-row-reverse" : ""}`}
+              style={isSender ? { gridArea: "1" } : {}}
             >
               <button
                 className={`${bubbleHover ? "inline-flex" : "hidden"} items-center gap-1 justify-center w-8 h-8 text-(--twitter-white) hover:bg-(--twitter-hover) rounded-full cursor-pointer`}
