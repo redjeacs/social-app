af4d49010f7b787e9d8bcf9cff6af163837bcaa3
Jerry Chang <213577976+redjeacs@users.noreply.github.com>
2026-07-07 15:42:14 +0800

    reconfigure socket to use produciton url

--- api/sockets/socket.js
+++ api/sockets/socket.js
@@ -7,8 +7,9 @@ module.exports = (app) => {
   const server = http.createServer(app);
   const io = new Server(server, {
     cors: {
-      origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+      origin: [process.env.CLIENT_URL, "http://localhost:5173"],
       methods: ["GET", "POST"],
+      credentials: true,
     },
     maxHttpBufferSize: 1e7, // 100 MB
   });
